86328289c4d8019c9a01c684741858438b7b660d
M. Rifqi Akram <rifqiakram28@gmail.com>
2024-03-09 10:10:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import joblib
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import re
+import sklearn
 
 # Load the best model from the .pkl file
 best_model = joblib.load('best_model.pkl')
@@ -55,9 +56,14 @@ def main():
     # Display prediction
     st.subheader('Prediction:')
 
-    # Menampilkan hasil prediksi berdasarkan teks yang dipilih
-    result_df = pd.DataFrame({'Predicted_Category': prediction})
-    st.table(result_df[result_df.index == test_df[test_df['text'] == selected_row].index])
+    def display_text_and_prediction(text, prediction):
+        result_df = pd.DataFrame({'Text': [text], 'Predicted_Category': [prediction]})
+        st.table(result_df[['Text', 'Predicted_Category']])
+
+    text_for_prediction = selected_row
+    
+    # Displaying text and prediction
+    display_text_and_prediction(text_for_prediction, prediction[0])
 
 if __name__ == '__main__':
     main()
